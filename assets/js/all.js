2913c8c2acc0fe6f5b0a5c5265f007e2511aa0b7
Kevin Chang <apog780212@gmail.com>
2022-11-21 13:11:18 +0800

    Update 2022-11-21T05:11:15.658Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,16 @@
 "use strict";
 
-/* global swal */
+var _AuthorizationHeader = _interopRequireDefault(require("./AuthorizationHeader.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+/* global axios */
+
+/* 訂閱區塊 */
+// 請自行更換 client_id 與 client_secret
+// GetApiResponse() 中 axios.get('URL') 的 URL 可替換成想要取得的網址。
+// eslint-disable-next-line import/extensions
+// API 認證，取得 token
 var subscriptionInfoForm = document.querySelector('.subscriptionInfo-form'); // 訂閱input輸入
 
 var subscriptionEmail = document.querySelector('.subscription-email'); // 訂閱按鈕
@@ -21,5 +31,111 @@ subscriptionBtn.addEventListener('click', function () {
     swal('訂閱成功！', '將定期發送相關觀光資訊', 'success');
     subscriptionInfoForm.reset();
   }
+}); // API 的 filter 用法：例如: 沒有圖片時
+// $filter=Picture/PictureUrl1 ne null
+// 宣告列表
+
+var tourList = document.querySelector('.tour-list'); // 存放觀光景點資料
+
+var tourData = []; // Modal
+
+var ScenicSpotModal = document.querySelector('#tourScenicSpotModal'); //  渲染列表
+
+function renderTourList(data) {
+  var str = ''; // console.log(data.length);
+
+  if (data.length === 0) {
+    str = "<li class=\"d-flex justify-content-center align-items-center\">\n    <span class=\"material-icons text-sm-m text-md-lg text-2xl me-4\">\n      error_outline\n    </span>\n    <p class=\"text-sm-m text-md-lg text-2xl text-center\">\u76EE\u524D\u6C92\u6709\u8CC7\u6599\n    </p>\n  </li>";
+    tourList.innerHTML = str;
+  } else {
+    data.forEach(function (item) {
+      // console.log(item);
+      // 若API有提供圖片網址，但圖片網址失效時，使用 onerror="this.src='https://i.ibb.co/5WGrGkK/404.jpg'" 替代
+      // if (JSON.stringify(item.Picture) === '{}') {
+      //   return;
+      // }
+      if (item.OpenTime === undefined) {
+        str += "<li class=\"col-md-6 col-lg-4 d-flex flex-column\">\n        <div class=\"card my-2 my-md-4 my-lg-6 card-shadow-hover h-100\">\n          <a href=\"\" class=\"stretched-link\" data-bs-toggle=\"modal\" data-bs-target=\"#tourScenicSpotModal\"\n            data-bs-whatever=\"".concat(item.ScenicSpotID, "\">\n            <img src=\"").concat(item.Picture.PictureUrl1, "\"\n              onerror=\"this.src='https://i.ibb.co/hR0Sb7y/404.jpg';this.onerror = null\"\n              class=\"card-img-top img-fluid\" alt=\".").concat(item.Picture.PictureDescription1, "\">\n          </a>\n          <div class=\"card-body\">\n            <h4 class=\"text-sm-m text-lg text-primary\">").concat(item.ScenicSpotName, "</h4>\n            <p class=\"text-sm-xs text-s text-primary mt-2\">\u958B\u653E\u6642\u9593\uFF1A\u672A\u63D0\u4F9B\u76F8\u95DC\u6642\u9593</p>\n            <p class=\"text-sm-xs text-s text-primary mt-2\">\u9023\u7D61\u96FB\u8A71\uFF1A").concat(item.Phone, "</p>\n          </div>\n        </div>\n      </li>");
+      } else {
+        str += "<li class=\"col-md-6 col-lg-4 d-flex flex-column\">\n        <div class=\"card my-2 my-md-4 my-lg-6 card-shadow-hover h-100\">\n          <a href=\"\" class=\"stretched-link\" data-bs-toggle=\"modal\" data-bs-target=\"#tourScenicSpotModal\"\n            data-bs-whatever=\"".concat(item.ScenicSpotID, "\">\n            <img src=\"").concat(item.Picture.PictureUrl1, "\"\n              onerror=\"this.src='https://i.ibb.co/hR0Sb7y/404.jpg';this.onerror = null\"\n              class=\"card-img-top img-fluid\" alt=\".").concat(item.Picture.PictureDescription1, "\">\n          </a>\n          <div class=\"card-body\">\n            <h4 class=\"text-sm-m text-lg text-primary\">").concat(item.ScenicSpotName, "</h4>\n            <p class=\"text-sm-xs text-s text-primary mt-2\">\u958B\u653E\u6642\u9593\uFF1A").concat(item.OpenTime, "</p>\n            <p class=\"text-sm-xs text-s text-primary mt-2\">\u9023\u7D61\u96FB\u8A71\uFF1A").concat(item.Phone, "</p>\n          </div>\n        </div>\n      </li>");
+      }
+    });
+    tourList.innerHTML = str;
+  }
+} // 取得預設景點資料
+
+
+function getAllTourList() {
+  var url = 'https://tdx.transportdata.tw/api/basic/v2/Tourism/ScenicSpot/Taipei?%24filter=contains%28Class1%2C%27%E9%81%8A%E6%86%A9%E9%A1%9E%27%29&%24top=6&%24skip=9&%24format=JSON';
+  axios.get(url, {
+    headers: (0, _AuthorizationHeader["default"])()
+  }).then(function (res) {
+    tourData = res.data;
+    renderTourList(tourData); // console.log(tourData);
+  })["catch"](function (error) {
+    console.log(error);
+  });
+} // 監聽
+
+
+ScenicSpotModal.addEventListener('show.bs.modal', function (e) {
+  // console.log(e.relatedTarget);
+  var modalBtn = e.relatedTarget; // 被點擊的元素可作為事件的 relatedTarget 屬性
+
+  var id = modalBtn.getAttribute('data-bs-whatever');
+  var img = ScenicSpotModal.querySelector('.card-img-top');
+  var title = ScenicSpotModal.querySelector('.card-title');
+  var description = ScenicSpotModal.querySelector('.card-text');
+  var openTime = ScenicSpotModal.querySelector('.openTime');
+  var phone = ScenicSpotModal.querySelector('.phone');
+  tourData.forEach(function (item) {
+    // console.log(item);
+    if (item.ScenicSpotID === id) {
+      img.setAttribute('src', "".concat(item.Picture.PictureUrl1));
+      title.textContent = "".concat(item.ScenicSpotName);
+      description.textContent = "".concat(item.DescriptionDetail);
+
+      if (item.OpenTime === undefined) {
+        // eslint-disable-next-line no-param-reassign
+        item.OpenTime = '未提供';
+      }
+
+      openTime.innerHTML = "\n        <span class=\"material-icons-outlined me-2\">\n          schedule\n        </span>\n        ".concat(item.OpenTime, "\n      ");
+      phone.innerHTML = "\n        <span class=\"material-icons me-2\">\n          call\n        </span>\n        <a href=\"tel:+".concat(item.Phone, "\">").concat(item.Phone, "</a>\n      ");
+    }
+  });
+}); // ------ 初始化
+
+function init() {
+  // 呼叫取得token函式
+  // GetAuthorizationHeader();
+  // 呼叫取得預設觀光景點資料
+  getAllTourList();
+}
+
+init();
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
+exports["default"] = GetAuthorizationHeader;
+
+/* global axios */
+// 請自行更換 client_id 與 client_secret
+// GetApiResponse() 中 axios.get('URL') 的 URL 可替換成想要取得的網址。
+// API 認證，取得 token
+function GetAuthorizationHeader() {
+  var authUrl = 'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token';
+  var parameter = {
+    grant_type: 'client_credentials',
+    client_id: 'apog780212-3c7005de-321a-4555',
+    client_secret: '4408c630-a7ff-4f57-8728-f4642ee5f932'
+  };
+  axios.post(authUrl, Qs.stringify(parameter)).then(function (res) {
+    axios.defaults.headers.common.Authorization = "Bearer ".concat(res.data.access_token);
+  })["catch"](function (err) {
+    console.log(err);
+  });
+}
 //# sourceMappingURL=all.js.map
